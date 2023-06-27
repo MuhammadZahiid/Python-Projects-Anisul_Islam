5f13fcccfa9a6468ccbd55fc40f74e20dbdd590d
Md. Zahid Hassan <130243388+MuhammadZahiid@users.noreply.github.com>
2023-06-27 21:51:20 +0600

    Add files via upload

---
+++ 11. formated string.py
@@ -0,0 +1,23 @@
+from math import *
+from string import *
+
+
+''' If i want to print a value in string
+then the string must be formated
+formate is f" here is the printed value in second bracket '''
+num = "10.5"
+
+print(" MD. ZAHID HASSAN ",end=(""))
+print(" 01521545594 ")
+
+
+
+print(type(num))
+
+num1 = 10
+num2 = 20
+print(f" {num1} + {num2} = {num1+num2}")
+
+num3 = int(input(" Tell first Number = "))
+num4 = int(input(" Tell Second Number = "))
+print( f" {num3} + {num4} = {num3+num4}")
